3509a486d9e2f2870602de6dc6b641f7e1c44b70
valenciaw <32558440+valenciaw@users.noreply.github.com>
2018-06-23 20:52:39 -0700

    Added function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function setup() {
   p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
-  myName.html("[Your Name Here]"); // enter your name here!
+  myName.html("Valencia"); // enter your name here!
 }
 
  /*
@@ -22,7 +22,34 @@ function setup() {
 // Write all your code inside the draw() function below!
 function draw() {
   // Example. You can remove when you're ready!
-  background(0, 255, 0); // try changing this to a blue background
+  //background("red");
   fill(255, 204, 0);    // Always call fill() before the shape you want to fill in.
   rect(30, 20, 55, 55); // Draw a 30x20 pixel rectangle at coordinate (55,55)
+fill(255, 204, 0)
+triangle(150, 195, 178, 150, 206, 195);
+drawTwoCircles();
+drawTriangle();
+drawMouseCircle();
+
+}
+
+function drawTwoCircles(){
+  fill("blue");
+  ellipse(500, 400, 55, 55);
+  ellipse(400, 400, 55, 55);
+
+}
+
+function drawTriangle(){
+  fill("pink");
+  triangle(250, 295, 278, 250, 306, 295);
+}
+
+function drawMouseCircle(){
+  if (mouseIsPressed) {
+    fill(117, 191, 47);
+  } else {
+    fill(210, 137, 249);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
